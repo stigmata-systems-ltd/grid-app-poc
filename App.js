f8071061a21bd72b9bf4465879d8f5451b09bbce
ananth2307 <beyonceptiontestmail@gmail.com>
2020-08-22 18:03:51 +0530

    Poc-Map

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Polygon } from 'react-native-maps';
+import gridData from "./gridData";
+import * as Location from 'expo-location';
 
 export default function App() {
+  const initialRegion = {
+    latitude: 12.994112,
+    longitude: 80.1686781,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      {/* Adding Provider prop for IOS support */}
+      <MapView
+        region={initialRegion}
+        style={styles.mapView}
+        provider={PROVIDER_GOOGLE}
+      >
+
+        {gridData.map(marker => {
+          return (
+            <>
+              <Polygon
+                key={"1"}
+                coordinates={marker.rectCords}
+              />
+              <Marker
+                key={marker.lat}
+                coordinate={{
+                  latitude: marker.lat,
+                  longitude: marker.lng
+                }}
+                title={marker.title}
+                description={marker.description}
+              />
+            </>
+          )
+        })}
+      </MapView>
+    </>
   );
 }
 
@@ -18,4 +54,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapView: {
+    height: "100%"
+  }
 });
---
+++ gridData.js
@@ -0,0 +1,77 @@
+export default gridData = [
+    {
+        lat: 12.992424,
+        lng: 80.151855,
+        title: "Grid-H1V1",
+        description: "Grid Test Desc1",
+        status: "inProgess",
+        rectCords: [
+            {
+                latitude: 12.993762,
+                longitude: 80.148808,
+            },
+            {
+                latitude: 12.994347,
+                longitude: 80.154259,
+            },
+            {
+                latitude: 12.990876,
+                longitude: 80.154731,
+            },
+            {
+                latitude: 12.990375,
+                longitude: 80.150010,
+            }
+        ]
+    },
+    {
+        lat: 12.993260,
+        lng: 80.156318,
+        title: "Grid-H1V2",
+        description: "Grid Test Desc2",
+        status: "inProgess",
+        rectCords: [
+            {
+                latitude: 12.994514,
+                longitude: 80.154387,
+            },
+            {
+                latitude: 12.994849,
+                longitude: 80.157949,
+            },
+            {
+                latitude: 12.991462,
+                longitude: 80.158507,
+            },
+            {
+                latitude: 12.990918,
+                longitude: 80.154945,
+            }
+        ]
+    },
+    {
+        lat: 12.989078,
+        lng: 80.152885,
+        title: "Grid-H1V3",
+        description: "Grid Test Desc2",
+        status: "inProgess",
+        rectCords: [
+            {
+                latitude: 12.990082,
+                longitude: 80.149924,
+            },
+            {
+                latitude: 12.990751,
+                longitude: 80.154774,
+            },
+            {
+                latitude: 12.987824,
+                longitude: 80.155160,
+            },
+            {
+                latitude: 12.986653,
+                longitude: 80.150782,
+            }
+        ]
+    },
+]
